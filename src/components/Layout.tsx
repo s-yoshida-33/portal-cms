41e26294357e20b417f2ec4db2953586d8dc449e
s-yoshida-33 <shota.yoshida@toeitechno.com>
2026-05-25 05:33:09 +0000

    feat: add fixed breadcrumb bar to layout; remove back links from project/device pages

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import { Sidebar } from './Sidebar';
 import { Footer } from './Footer';
+import { Breadcrumbs } from './Breadcrumbs';
 
 export function Layout() {
   return (
     <div className="flex h-screen bg-black">
       <Sidebar />
-      <main className="flex-1 flex flex-col overflow-auto">
-        <div className="flex-1">
-          <Outlet />
-        </div>
-        <Footer />
-      </main>
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Breadcrumbs />
+        <main className="flex-1 flex flex-col overflow-auto">
+          <div className="flex-1">
+            <Outlet />
+          </div>
+          <Footer />
+        </main>
+      </div>
     </div>
   );
 }
